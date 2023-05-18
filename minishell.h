dd076cc0dcae5252a5a2689fc500f904ff57d272
k16aO2digOOh <abdlkadraoutem@gmail.com>
2023-05-18 20:38:42 +0100

    besmlah

---
+++ minishell.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaoutem- <aaoutem-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/18 20:32:34 by aaoutem-          #+#    #+#             */
+/*   Updated: 2023/05/18 20:37:26 by aaoutem-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H_
+# define MINISHELL_H_
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+# include <sys/stat.h>
+# include <dirent.h>
+# include <string.h>
+
+
+
+#endif
