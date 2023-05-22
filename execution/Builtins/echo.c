873470b525fe6191fb2cf2569d50ea36a7d64039
Kadigh53 <abdlkadraoutem@gmail.com>
2023-05-22 02:02:02 +0100

    imik yadn

--- execution/Builtins/echo.c
+++ execution/Builtins/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaoutem- <aaoutem-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kadigh <kadigh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 13:06:37 by aaoutem-          #+#    #+#             */
-/*   Updated: 2023/05/21 19:34:06 by aaoutem-         ###   ########.fr       */
+/*   Updated: 2023/05/22 01:59:28 by kadigh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,26 @@
 
 void    echo(t_env **env, char **args)
 {
+	int i;
+	int fd;
+	bool n;
+
+	i = 1;
+	n = false;
+	if (ft_strcmp(args[0], "-n",  2) == 0)
+	{
+		while (args[0][i] && args[0][i] == 'n')
+			i++;
+		if (i == ft_strlen(args[0]))
+			n = true;
+	}
+	i = 0;
+	if (n)
+		i = 1;
+	while (args[i])
+	{
+		write(fd, args, ft_strlen(args));
+	}
+	if (!n)
+		write(fd, "\n", 1);
 }
